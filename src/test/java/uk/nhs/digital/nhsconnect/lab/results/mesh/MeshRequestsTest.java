1727ba8ad1eb523cadc0251a0b2354c14d762cd8
n-r-b <nblanchette@companieshouse.gov.uk>
2021-01-08 16:29:41 +0000

    NIAD-828: refactor hard coded values in MeshRequestsTests.java

--- src/test/java/uk/nhs/digital/nhsconnect/lab/results/mesh/MeshRequestsTest.java
+++ src/test/java/uk/nhs/digital/nhsconnect/lab/results/mesh/MeshRequestsTest.java
@@ -16,10 +16,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class MeshRequestsTest {
 
-    private final MeshConfig meshConfig = new MeshConfig("mailboxId",
+    private static final String MESH_HOST = "https://localhost:8829/messageexchange/";
+    private static final String MAILBOX_ID = "mailboxId";
+    private static final String MESSAGE_ID = "messageId";
+    private static final String MESSAGE_RECIPIENT = "recipient";
+
+    private final MeshConfig meshConfig = new MeshConfig(MAILBOX_ID,
         "password",
         "SharedKey",
-        "https://localhost:8829/messageexchange/",
+            MESH_HOST,
         "false",
         StringUtils.EMPTY,
         StringUtils.EMPTY,
@@ -30,30 +35,28 @@ class MeshRequestsTest {
     void when_GettingMessage_Then_ExpectHttpGetAndCorrectUri() {
         MeshRequests meshRequests = new MeshRequests(meshConfig, meshHeaders);
 
-        var request = meshRequests.getMessage("messageId");
+        var request = meshRequests.getMessage(MESSAGE_ID);
 
         assertThat(request).isExactlyInstanceOf(HttpGet.class);
-        assertThat(request.getURI().toString()).isEqualTo("https://localhost:8829/messageexchange/mailboxId/inbox/messageId");
+        assertThat(request.getURI().toString()).isEqualTo(MESH_HOST + MAILBOX_ID + "/inbox/" + MESSAGE_ID);
     }
 
     @Test
     void when_SendingRegistrationMessage_Then_ExpectHttpPostAndCorrectUri() {
         MeshRequests meshRequests = new MeshRequests(meshConfig, meshHeaders);
 
-        String recipient = "recipient";
-        var request = meshRequests.sendMessage(recipient, WorkflowId.REGISTRATION);
+        var request = meshRequests.sendMessage(MESSAGE_RECIPIENT, WorkflowId.REGISTRATION);
 
-        assertSending(request, recipient, WorkflowId.REGISTRATION);
+        assertSending(request, WorkflowId.REGISTRATION);
     }
 
     @Test
     void when_SendingRecepMessage_Then_ExpectHttpPostAndCorrectUri() {
         MeshRequests meshRequests = new MeshRequests(meshConfig, meshHeaders);
 
-        String recipient = "recipient";
-        var request = meshRequests.sendMessage(recipient, WorkflowId.RECEP);
+        var request = meshRequests.sendMessage(MESSAGE_RECIPIENT, WorkflowId.RECEP);
 
-        assertSending(request, recipient, WorkflowId.RECEP);
+        assertSending(request, WorkflowId.RECEP);
     }
 
     @Test
@@ -63,17 +66,17 @@ class MeshRequestsTest {
         var request = meshRequests.getMessageIds();
 
         assertThat(request).isExactlyInstanceOf(HttpGet.class);
-        assertThat(request.getURI().toString()).isEqualTo("https://localhost:8829/messageexchange/mailboxId/inbox");
+        assertThat(request.getURI().toString()).isEqualTo(MESH_HOST + MAILBOX_ID + "/inbox");
     }
 
     @Test
     void when_AcknowledgeMessage_Then_ExpectHttpPutAndCorrectUri() {
         MeshRequests meshRequests = new MeshRequests(meshConfig, meshHeaders);
 
-        var request = meshRequests.acknowledge("messageId");
+        var request = meshRequests.acknowledge(MESSAGE_ID);
 
         assertThat(request).isExactlyInstanceOf(HttpPut.class);
-        assertThat(request.getURI().toString()).isEqualTo("https://localhost:8829/messageexchange/mailboxId/inbox/messageId/status/acknowledged");
+        assertThat(request.getURI().toString()).isEqualTo(MESH_HOST + MAILBOX_ID + "/inbox/" +  MESSAGE_ID + "/status/acknowledged");
     }
 
     @Test
@@ -83,15 +86,15 @@ class MeshRequestsTest {
         var request = meshRequests.authenticate();
 
         assertThat(request).isExactlyInstanceOf(HttpPost.class);
-        assertThat(request.getURI().toString()).isEqualTo("https://localhost:8829/messageexchange/mailboxId");
+        assertThat(request.getURI().toString()).isEqualTo(MESH_HOST + MAILBOX_ID);
     }
 
-    private void assertSending(HttpEntityEnclosingRequestBase request, String recipient, WorkflowId workflowId) {
+    private void assertSending(HttpEntityEnclosingRequestBase request, WorkflowId workflowId) {
         assertThat(request).isExactlyInstanceOf(HttpPost.class);
-        assertThat(request.getURI().toString()).isEqualTo("https://localhost:8829/messageexchange/mailboxId/outbox");
+        assertThat(request.getURI().toString()).isEqualTo(MESH_HOST + MAILBOX_ID  + "/outbox");
         Header[] mexToHeader = request.getHeaders("Mex-To");
         assertThat(mexToHeader.length).isEqualTo(1);
-        assertThat(mexToHeader[0].getValue()).isEqualTo(recipient);
+        assertThat(mexToHeader[0].getValue()).isEqualTo(MESSAGE_RECIPIENT);
         assertThat(request.getHeaders("Mex-WorkflowID")[0].getValue()).isEqualTo(workflowId.getWorkflowId());
     }
 }
