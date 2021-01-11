6b914b2f40d88ea1cfb226de3777bcef0fa1af91
Sailesh Ashokbhai Patel <s.patel@kainos.com>
2021-01-11 15:17:42 +0000

    NIAD-848: add whitespace between fields so annotations are easier to understand

--- src/main/java/uk/nhs/digital/nhsconnect/lab/results/model/edifact/Message.java
+++ src/main/java/uk/nhs/digital/nhsconnect/lab/results/model/edifact/Message.java
@@ -12,19 +12,23 @@ public class Message extends Section {
     private static final String DEFAULT_GP_CODE = "9999";
 
     @Getter(lazy = true)
-    private final uk.nhs.digital.nhsconnect.lab.results.model.edifact.MessageHeader messageHeader =
-        uk.nhs.digital.nhsconnect.lab.results.model.edifact.MessageHeader.fromString(extractSegment(MessageHeader.KEY));
+    private final MessageHeader messageHeader =
+        MessageHeader.fromString(extractSegment(MessageHeader.KEY));
+
     @Getter(lazy = true)
     private final HealthAuthorityNameAndAddress healthAuthorityNameAndAddress =
         HealthAuthorityNameAndAddress.fromString(extractSegment(HealthAuthorityNameAndAddress.KEY_QUALIFIER));
+
     @Getter(lazy = true)
     private final ReferenceTransactionType referenceTransactionType =
         ReferenceTransactionType.fromString(extractSegment(ReferenceTransactionType.KEY_QUALIFIER));
+
     @Getter(lazy = true)
     private final List<ReferenceMessageRecep> referenceMessageReceps =
         extractSegments(ReferenceMessageRecep.KEY_QUALIFIER).stream()
             .map(ReferenceMessageRecep::fromString)
             .collect(Collectors.toList());
+
     @Getter(lazy = true)
     private final ReferenceInterchangeRecep referenceInterchangeRecep =
         ReferenceInterchangeRecep.fromString(extractSegment(ReferenceInterchangeRecep.KEY_QUALIFIER));
