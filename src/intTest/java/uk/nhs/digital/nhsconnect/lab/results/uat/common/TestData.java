9905ffcd36b618eef00e580caf6abf52ed577880
Sailesh Ashokbhai Patel <s.patel@kainos.com>
2021-01-11 17:34:30 +0000

    NIAD-848: add Java dependencies for InboundArgumentsProvider.java

---
+++ src/intTest/java/uk/nhs/digital/nhsconnect/lab/results/uat/common/TestData.java
@@ -0,0 +1,12 @@
+package uk.nhs.digital.nhsconnect.lab.results.uat.common;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class TestData {
+    private final String edifact;
+    private final String recep;
+    private final String json;
+}
