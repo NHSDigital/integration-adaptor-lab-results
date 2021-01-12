83b531445b562a01df00040d0700b6340cd0c9fc
antonlebedjko <antonio96@inbox.lv>
2021-01-12 15:34:37 +0000

    Add InboundQueueService
    Implementing the publish method in InboundQueueService class which is in charge of putting a message on inbound queue

---
+++ src/main/java/uk/nhs/digital/nhsconnect/lab/results/inbound/queue/InboundQueueService.java
@@ -0,0 +1,48 @@
+package uk.nhs.digital.nhsconnect.lab.results.inbound.queue;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+import uk.nhs.digital.nhsconnect.lab.results.mesh.message.InboundMeshMessage;
+import uk.nhs.digital.nhsconnect.lab.results.utils.ConversationIdService;
+import uk.nhs.digital.nhsconnect.lab.results.utils.JmsHeaders;
+import uk.nhs.digital.nhsconnect.lab.results.utils.TimestampService;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class InboundQueueService {
+
+    private final ObjectMapper objectMapper;
+
+    private final TimestampService timestampService;
+
+    private final JmsTemplate jmsTemplate;
+
+    private final ConversationIdService conversationIdService;
+
+    @Value("${labresults.amqp.meshInboundQueueName}")
+    private String meshInboundQueueName;
+
+    @SneakyThrows
+    public void publish(InboundMeshMessage messageContent) {
+        String meshInboundQueueName = "lab";
+        messageContent.setMessageSentTimestamp(timestampService.formatInISO(timestampService.getCurrentTimestamp()));
+        jmsTemplate.send(meshInboundQueueName, session -> {
+            var message = session.createTextMessage(serializeMeshMessage(messageContent));
+            message.setStringProperty(JmsHeaders.CONVERSATION_ID, conversationIdService.getCurrentConversationId());
+            return message;
+        });
+    }
+
+    @SneakyThrows
+    private String serializeMeshMessage(InboundMeshMessage meshMessage) {
+        return objectMapper.writeValueAsString(meshMessage);
+    }
+}
--- src/test/java/uk/nhs/digital/nhsconnect/lab/results/IntegrationAdapterLabResultsApplicationTests.java
+++ src/test/java/uk/nhs/digital/nhsconnect/lab/results/IntegrationAdapterLabResultsApplicationTests.java
@@ -1,5 +1,6 @@
 package uk.nhs.digital.nhsconnect.lab.results;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
