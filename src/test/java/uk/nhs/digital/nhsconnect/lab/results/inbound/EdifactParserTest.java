dd40d7a49e0a3a4e2f8e7bb804a4ba0e82bda312
Sailesh Ashokbhai Patel <s.patel@kainos.com>
2021-01-14 14:37:46 +0000

    NIAD-848: test if exception is thrown when header and trailer are not in order

--- src/test/java/uk/nhs/digital/nhsconnect/lab/results/inbound/EdifactParserTest.java
+++ src/test/java/uk/nhs/digital/nhsconnect/lab/results/inbound/EdifactParserTest.java
@@ -14,6 +14,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.nhs.digital.nhsconnect.lab.results.model.edifact.Interchange;
 import uk.nhs.digital.nhsconnect.lab.results.model.edifact.message.InterchangeFactory;
+import uk.nhs.digital.nhsconnect.lab.results.model.edifact.message.ToEdifactParsingException;
 
 import java.util.List;
 
@@ -51,6 +52,35 @@ class EdifactParserTest {
             EDIFACT_TRAILER + "'"
     );
 
+    private static final List<String> TRAILER_BEFORE_HEADER = List.of(
+            EDIFACT_HEADER + "'",
+            "UNT+24+00000004'",
+            "BGM+++507'",
+            "NAD+FHS+XX1:954'",
+            "DTM+137:199201141619:203'",
+            "RFF+950:F4'",
+            "RFF+TN:18'",
+            "S01+1'",
+            "NAD+GP+2750922,295:900'",
+            "NAD+RIC+RT:956'",
+            "QTY+951:6'",
+            "QTY+952:3'",
+            "HEA+ACD+A:ZZZ'",
+            "HEA+ATP+2:ZZZ'",
+            "HEA+BM+S:ZZZ'",
+            "HEA+DM+Y:ZZZ'",
+            "DTM+956:19920114:102'",
+            "LOC+950+GLASGOW'",
+            "FTX+RGI+++BABY AT THE REYNOLDS-THORPE CENTRE'",
+            "S02+2'",
+            "PNA+PAT+NHS123:OPI+++SU:KENNEDY+FO:SARAH+TI:MISS+MI:ANGELA'",
+            "DTM+329:19911209:102'",
+            "PDI+2'",
+            "NAD+PAT++??:26 FARMSIDE CLOSE:ST PAULS CRAY:ORPINGTON:KENT+++++BR6  7ET'",
+            "UNH+00000004+FHSREG:0:1:FH:FHS001'",
+            EDIFACT_TRAILER + "'"
+    );
+
     @Mock
     private InterchangeFactory interchangeFactory;
 
@@ -70,6 +100,14 @@ class EdifactParserTest {
 
         // trailing empty string because we split by apostrophe and there's a trailing apostrophe
         verify(interchangeFactory).createInterchange(List.of(EDIFACT_HEADER, EDIFACT_TRAILER, ""));
+    }
+
+    @Test
+    void testParsePropagatesExceptionWhenPassedTrailerBeforeHeader() {
+        when(interchangeFactory.createInterchange(any())).thenReturn(interchange);
+
+        assertThrows(ToEdifactParsingException.class,
+                () -> edifactParser.parse(String.join("\n", TRAILER_BEFORE_HEADER)));
     }
 
     @Test
