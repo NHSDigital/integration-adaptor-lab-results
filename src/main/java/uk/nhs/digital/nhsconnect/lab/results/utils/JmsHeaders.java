5a86579a1655191e591b9a6229258cceef20a483
antonlebedjko <antonio96@inbox.lv>
2021-01-18 17:02:08 +0000

    Remove unused JmsHeaders

--- src/main/java/uk/nhs/digital/nhsconnect/lab/results/utils/JmsHeaders.java
+++ src/main/java/uk/nhs/digital/nhsconnect/lab/results/utils/JmsHeaders.java
@@ -1,7 +1,5 @@
 package uk.nhs.digital.nhsconnect.lab.results.utils;
 
 public final class JmsHeaders {
-    public static String OPERATION_ID = "OperationId";
-    public static String TRANSACTION_TYPE = "TransactionType";
     public static String CONVERSATION_ID = "ConversationId";
 }
