52533c6eae28771d24147b1633fd578dceafc44e
antonlebedjko <antonio96@inbox.lv>
2021-01-18 17:19:07 +0000

    Remove unused import from MeshServiceTest

--- src/test/java/uk/nhs/digital/nhsconnect/lab/results/mesh/MeshServiceTest.java
+++ src/test/java/uk/nhs/digital/nhsconnect/lab/results/mesh/MeshServiceTest.java
@@ -1,7 +1,6 @@
 package uk.nhs.digital.nhsconnect.lab.results.mesh;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
