f8c15d6f0fc581a1fd320965ac3d2a9a1fe11784
antonlebedjko <36967438+antonlebedjko@users.noreply.github.com>
2021-01-18 12:46:13 +0000

    Update src/test/java/uk/nhs/digital/nhsconnect/lab/results/inbound/queue/InboundQueueServiceTest.java
    Co-authored-by: Andy Walker <andrewwa@kainos.com>

--- src/test/java/uk/nhs/digital/nhsconnect/lab/results/inbound/queue/InboundQueueServiceTest.java
+++ src/test/java/uk/nhs/digital/nhsconnect/lab/results/inbound/queue/InboundQueueServiceTest.java
@@ -21,7 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class InboundQueueServiceTest {
+class InboundQueueServiceTest {
 
     public static final String CONVERSATION_ID = "CONV123";
 
