7dd188ba0264bd8b9446d2b1480e976767e56204
antonlebedjko <antonio96@inbox.lv>
2021-01-18 12:42:38 +0000

    Remove unnecessary @EnableScheduling from IntegrationAdapterLabResultsApplicationTests

--- src/test/java/uk/nhs/digital/nhsconnect/lab/results/IntegrationAdapterLabResultsApplicationTests.java
+++ src/test/java/uk/nhs/digital/nhsconnect/lab/results/IntegrationAdapterLabResultsApplicationTests.java
@@ -2,9 +2,7 @@ package uk.nhs.digital.nhsconnect.lab.results;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootTest
 class IntegrationAdapterLabResultsApplicationTests {
 
