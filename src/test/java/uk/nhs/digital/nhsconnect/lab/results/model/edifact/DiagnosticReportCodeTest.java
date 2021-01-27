6e7c9a87943db628a04c3fc082fe5cf8f39ba761
Sailesh Ashokbhai Patel <s.patel@kainos.com>
2021-01-27 14:05:22 +0000

    NIAD-1008: test DiagnosticReportCode fromString with valid & invalid input

--- src/test/java/uk/nhs/digital/nhsconnect/lab/results/model/edifact/DiagnosticReportCodeTest.java
+++ src/test/java/uk/nhs/digital/nhsconnect/lab/results/model/edifact/DiagnosticReportCodeTest.java
@@ -29,4 +29,18 @@ public class DiagnosticReportCodeTest {
         assertThatThrownBy(diagnosticReportCode::toEdifact).isInstanceOf(EdifactValidationException.class);
     }
 
+    @Test
+    void testFromStringWithValidInput() {
+        DiagnosticReportCode diagnosticReportCode = DiagnosticReportCode.fromString("GIS+N");
+        DiagnosticReportCode expectedDiagnosticReportCode = new DiagnosticReportCode("N");
+
+        assertThat(diagnosticReportCode.getValue()).isEqualTo(expectedDiagnosticReportCode.getValue());
+
+    }
+
+    @Test
+    void testFromStringWithInvalidInput() {
+        assertThatThrownBy(() -> DiagnosticReportCode.fromString("XXX+N"))
+            .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
 }
