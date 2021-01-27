dbf3e6b6bd39b2fe7c72c19dbd2120a22daf1925
Sailesh Ashokbhai Patel <s.patel@kainos.com>
2021-01-27 13:23:58 +0000

    NIAD-1008: added implementation to segment method stubs
    Added implementations for the methods inherited from Segment

--- src/main/java/uk/nhs/digital/nhsconnect/lab/results/model/edifact/DiagnosticReportCode.java
+++ src/main/java/uk/nhs/digital/nhsconnect/lab/results/model/edifact/DiagnosticReportCode.java
@@ -1,25 +1,49 @@
 package uk.nhs.digital.nhsconnect.lab.results.model.edifact;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NonNull;
 import uk.nhs.digital.nhsconnect.lab.results.model.edifact.message.EdifactValidationException;
+import uk.nhs.digital.nhsconnect.lab.results.model.edifact.message.Split;
 
 /**
  * Example GIS+N'
  */
+@EqualsAndHashCode(callSuper = false)
+@Builder
+@Getter
+@AllArgsConstructor
 public class DiagnosticReportCode extends Segment {
+
+    public final static String KEY = "GIS";
+    private final @NonNull String code;
+
+    public static DiagnosticReportCode fromString(String edifactString) {
+        if (!edifactString.startsWith(DiagnosticReportCode.KEY)) {
+            throw new IllegalArgumentException("Can't create " + DiagnosticReportCode.class.getSimpleName() + " from " + edifactString);
+        }
+        String[] keySplit = Split.byPlus(edifactString);
+        return new DiagnosticReportCode(keySplit[1]);
+    }
 
     @Override
     public String getKey() {
-        return null;
+        return KEY;
     }
 
     @Override
     public String getValue() {
-        return null;
+        return code;
     }
 
     @Override
     protected void validateStateful() throws EdifactValidationException {
+        if(code.isBlank()) {
+            throw new EdifactValidationException(getKey() + ": Diagnostic Report Code is required");
 
+        }
     }
 
     @Override
