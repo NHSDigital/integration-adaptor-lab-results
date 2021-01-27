c5a87d7520da5a5e94afa18713be4c6f3276b0c6
Sailesh Ashokbhai Patel <s.patel@kainos.com>
2021-01-27 14:50:39 +0000

    NIAD-1008: add fields for ReferenceDiagnosticReport segment
    Add KEY, QUALIFIER and KEY_QUALIFIER fields
    Add referenceNumber field to obtain value of Edifact String

--- src/main/java/uk/nhs/digital/nhsconnect/lab/results/model/edifact/ReferenceDiagnosticReport.java
+++ src/main/java/uk/nhs/digital/nhsconnect/lab/results/model/edifact/ReferenceDiagnosticReport.java
@@ -1,8 +1,23 @@
 package uk.nhs.digital.nhsconnect.lab.results.model.edifact;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import uk.nhs.digital.nhsconnect.lab.results.model.edifact.message.EdifactValidationException;
 
+/**
+ * Example RFF+SRI:15/CH000037K/200010191704'
+ */
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class ReferenceDiagnosticReport extends Segment {
+
+    public static final String KEY = "RFF";
+    public static final String QUALIFIER = "SRI";
+    public static final String KEY_QUALIFIER = KEY + "+" + QUALIFIER;
+
+    private final String referenceNumber;
 
     @Override
     public String getKey() {
