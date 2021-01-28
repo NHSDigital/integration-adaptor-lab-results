c645fcfdf4b5fa6a21bcb3fd5f7fa4b3fe3f33a0
Sailesh Ashokbhai Patel <s.patel@kainos.com>
2021-01-28 16:07:53 +0000

    NIAD-1008: create DiagnosticReportStatus segment with tests
    - Created a Segment for DiagnosticReportStatus (STS)
    - Created unit tests for DiagnosticReportStatus

---
+++ src/main/java/uk/nhs/digital/nhsconnect/lab/results/model/edifact/DiagnosticReportStatus.java
@@ -0,0 +1,52 @@
+package uk.nhs.digital.nhsconnect.lab.results.model.edifact;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import uk.nhs.digital.nhsconnect.lab.results.model.edifact.message.EdifactValidationException;
+import uk.nhs.digital.nhsconnect.lab.results.model.edifact.message.Split;
+
+/**
+ * Example STS++UN'
+ */
+@EqualsAndHashCode(callSuper = false)
+@Builder
+@Getter
+@AllArgsConstructor
+public class DiagnosticReportStatus extends Segment {
+
+    private static final String KEY = "STS";
+
+    private final String event;
+
+    public static DiagnosticReportStatus fromString(final String edifactString) {
+        if (!edifactString.startsWith(KEY)) {
+            throw new IllegalArgumentException("Can't create " + DiagnosticReportStatus.class.getSimpleName() + " from " + edifactString);
+        }
+        String value = Split.bySegmentTerminator(Split.byPlus(edifactString)[2])[0];
+        return new DiagnosticReportStatus(value);
+    }
+
+    @Override
+    public String getKey() {
+        return KEY;
+    }
+
+    @Override
+    public String getValue() {
+        return event;
+    }
+
+    @Override
+    protected void validateStateful() throws EdifactValidationException {
+
+    }
+
+    @Override
+    public void preValidate() throws EdifactValidationException {
+        if (event.isBlank()) {
+            throw new EdifactValidationException(getKey() + ": Status is required");
+        }
+    }
+}
---
+++ src/test/java/uk/nhs/digital/nhsconnect/lab/results/model/edifact/DiagnosticReportStatusTest.java
@@ -0,0 +1,54 @@
+package uk.nhs.digital.nhsconnect.lab.results.model.edifact;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import uk.nhs.digital.nhsconnect.lab.results.model.edifact.message.EdifactValidationException;
+
+public class DiagnosticReportStatusTest {
+
+    private static final String VALID_EDIFACT = "STS++UN'";
+    private static final String VALID_EDIFACT_VALUE = "UN";
+
+
+    @Test
+    void testToEdifactWithValidDiagnosticReportStatus() {
+        final DiagnosticReportStatus diagnosticReportStatus = DiagnosticReportStatus.builder()
+            .event("+UN")
+            .build();
+
+        final String actualValue = diagnosticReportStatus.toEdifact();
+
+        assertEquals(VALID_EDIFACT, actualValue);
+    }
+
+    @Test
+    void testToEdifactWithInvalidDiagnosticReportStatus() {
+        final DiagnosticReportStatus diagnosticReportStatus = DiagnosticReportStatus.builder()
+            .event("")
+            .build();
+
+        final EdifactValidationException exception = assertThrows(EdifactValidationException.class,
+            diagnosticReportStatus::toEdifact);
+
+        assertEquals("STS: Status is required", exception.getMessage());
+    }
+
+    @Test
+    void testFromStringWithValidEdifactStringReturnsDiagnosticReportStatus() {
+        final DiagnosticReportStatus diagnosticReportStatus = DiagnosticReportStatus.fromString(VALID_EDIFACT);
+
+        assertEquals("STS", diagnosticReportStatus.getKey());
+        assertEquals(VALID_EDIFACT_VALUE, diagnosticReportStatus.getValue());
+    }
+
+    @Test
+    void testFromStringWithInvalidEdifactStringThrowsException() {
+        final IllegalArgumentException exception =
+            assertThrows(IllegalArgumentException.class, () -> DiagnosticReportStatus.fromString("wrong value"));
+
+        assertEquals("Can't create DiagnosticReportStatus from wrong value", exception.getMessage());
+    }
+}
