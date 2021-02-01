fb1e00283c81abddf11e087a754cbd492fe7ee7e
Andrew Walker <andrewwa@kainos.com>
2021-02-01 14:40:54 +0000

    Add ReferenceServiceSubject for segment 6

---
+++ src/main/java/uk/nhs/digital/nhsconnect/lab/results/model/edifact/ReferenceServiceSubject.java
@@ -0,0 +1,58 @@
+package uk.nhs.digital.nhsconnect.lab.results.model.edifact;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
+import uk.nhs.digital.nhsconnect.lab.results.model.edifact.message.EdifactValidationException;
+import uk.nhs.digital.nhsconnect.lab.results.model.edifact.message.Split;
+
+/**
+ * E.g. {@code}RFF+SSI:X88442211'{@code}
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReferenceServiceSubject extends Segment {
+    private static final String KEY = "RFF";
+    private static final String QUALIFIER = "SSI";
+    private static final String KEY_QUALIFIER = KEY + "+" + QUALIFIER;
+
+    @NonNull
+    private String number;
+
+    public static ReferenceServiceSubject fromString(final String edifact) {
+        if (!edifact.startsWith(KEY_QUALIFIER)) {
+            throw new IllegalArgumentException("Can't create " + ReferenceServiceSubject.class.getSimpleName()
+                + " from " + edifact);
+        }
+
+        final String[] split = Split.byColon(edifact);
+        return new ReferenceServiceSubject(split[1]);
+    }
+
+    @Override
+    public String getKey() {
+        return KEY;
+    }
+
+    @Override
+    public String getValue() {
+        return QUALIFIER + ":" + number;
+    }
+
+    @Override
+    protected void validateStateful() throws EdifactValidationException {
+        // no op
+    }
+
+    @Override
+    public void preValidate() throws EdifactValidationException {
+        if (StringUtils.isEmpty(number)) {
+            throw new EdifactValidationException(KEY + ": Attribute number is required");
+        }
+    }
+}
