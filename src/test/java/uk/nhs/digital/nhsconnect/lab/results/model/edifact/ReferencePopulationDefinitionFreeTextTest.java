b62b7ff300f95acf40dedee1d53dd665adf566e0
Andrew Walker <andrewwa@kainos.com>
2021-02-02 11:24:52 +0000

    Add SG20 segment code
    * RangeDetail is used to define lower and upper bounds for
    numeric data. At least one of the bounds must be given.
    * ReferencePopulationDefinitionFreeText is a qualified form
    of a FreeText segment, allowing between 1 and 5 (inc.)
    strings intended for population definitions.

---
+++ src/test/java/uk/nhs/digital/nhsconnect/lab/results/model/edifact/ReferencePopulationDefinitionFreeTextTest.java
@@ -0,0 +1,57 @@
+package uk.nhs.digital.nhsconnect.lab.results.model.edifact;
+
+import org.junit.jupiter.api.Test;
+import uk.nhs.digital.nhsconnect.lab.results.model.edifact.message.EdifactValidationException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class ReferencePopulationDefinitionFreeTextTest {
+    @Test
+    void testGetKey() {
+        assertThat(new ReferencePopulationDefinitionFreeText(new String[0]).getKey())
+            .isEqualTo("FTX");
+    }
+
+    @Test
+    void testWrongKey() {
+        assertThatThrownBy(() -> ReferencePopulationDefinitionFreeText.fromText("WRONG+RPD+++OK"))
+            .isExactlyInstanceOf(IllegalArgumentException.class)
+            .hasMessage("Can't create ReferencePopulationDefinitionFreeText from WRONG+RPD+++OK");
+    }
+
+    @Test
+    void testNoTexts() {
+        var result = ReferencePopulationDefinitionFreeText.fromText("FTX+RPD+++");
+        assertAll(
+            () -> assertThat(result.getValue()).isEqualTo("RPD+++"),
+            () -> assertThat(result.getFreeTexts()).isEmpty(),
+            () -> assertThatThrownBy(result::preValidate)
+                .isExactlyInstanceOf(EdifactValidationException.class)
+                .hasMessage("FTX+RPD: At least one reference population definition must be given."),
+            () -> assertThatNoException().isThrownBy(result::validateStateful)
+        );
+    }
+
+    @Test
+    void testOneText() {
+        var result = ReferencePopulationDefinitionFreeText.fromText("FTX+RPD+++Okay");
+        assertAll(
+            () -> assertThat(result.getValue()).isEqualTo("RPD+++Okay"),
+            () -> assertThat(result.getFreeTexts()).containsExactly("Okay"),
+            () -> assertThatNoException().isThrownBy(result::preValidate),
+            () -> assertThatNoException().isThrownBy(result::validateStateful)
+        );
+    }
+
+    @Test
+    @SuppressWarnings("checkstyle:MagicNumber")
+    void testTooManyFreeTexts() {
+        assertThatThrownBy(
+            () -> ReferencePopulationDefinitionFreeText.fromText("FTS+RPD+++A" + ":".repeat(10)))
+            .isExactlyInstanceOf(IllegalArgumentException.class)
+            .hasMessage("Can't create ReferencePopulationDefinitionFreeText from FTS+RPD+++A::::::::::");
+    }
+}
