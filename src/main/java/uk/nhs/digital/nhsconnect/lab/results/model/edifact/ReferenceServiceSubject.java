b340c8b4bb8862bd16dc5c7200cf609cee6cb96f
Andrew Walker <andrewwa@kainos.com>
2021-02-02 09:49:03 +0000

    Fix javadoc code annotation
    First I'd mistook the `{@code}` taglet as working kind of like
    XML, with opening and closing parts. Turns out it's just meant
    to wrap the content like `{@code content goes here}`.

--- src/main/java/uk/nhs/digital/nhsconnect/lab/results/model/edifact/ReferenceServiceSubject.java
+++ src/main/java/uk/nhs/digital/nhsconnect/lab/results/model/edifact/ReferenceServiceSubject.java
@@ -11,7 +11,7 @@ import uk.nhs.digital.nhsconnect.lab.results.model.edifact.message.EdifactValida
 import uk.nhs.digital.nhsconnect.lab.results.model.edifact.message.Split;
 
 /**
- * E.g. {@code}RFF+SSI:X88442211'{@code}
+ * E.g. {@code RFF+SSI:X88442211'}
  */
 @EqualsAndHashCode(callSuper = true)
 @Getter
