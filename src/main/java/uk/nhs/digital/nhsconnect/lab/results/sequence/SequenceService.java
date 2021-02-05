b862633434c48ca9b87aef9c9c18e4726f9e81bf
Sailesh Ashokbhai Patel <s.patel@kainos.com>
2021-02-05 10:05:17 +0000

    NIAD-1018: remove Transaction from Sequence

--- src/main/java/uk/nhs/digital/nhsconnect/lab/results/sequence/SequenceService.java
+++ src/main/java/uk/nhs/digital/nhsconnect/lab/results/sequence/SequenceService.java
@@ -6,17 +6,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SequenceService {
-    private static final String TRANSACTION_KEY_FORMAT = "TN-%s";
     private static final String INTERCHANGE_FORMAT = "SIS-%s-%s";
     private static final String INTERCHANGE_MESSAGE_FORMAT = "SMS-%s-%s";
 
     @Autowired
     private SequenceRepository sequenceRepository;
-
-    public Long generateTransactionNumber(final String generalPractitioner) {
-        validateSender(generalPractitioner);
-        return sequenceRepository.getNextForTransaction(String.format(TRANSACTION_KEY_FORMAT, generalPractitioner));
-    }
 
     public Long generateInterchangeSequence(final String sender, final String recipient) {
         validateSenderAndRecipient(sender, recipient);
