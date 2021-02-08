08b4fec987b342d738930236c01fd028da50e1e5
Andrew Walker <andrewwa@kainos.com>
2021-02-08 16:06:50 +0000

    Add mapping from edifact Requester to FHIR Practitioner
    The RequesterMapper now pulls out the relevant info
    from a Message to create a Practitioner, if possible.
    The EdifactToFhirService uses the RequesterMapper and
    if a Practitioner is indeed returned, adds it as a
    parameter to the outbound FHIR.
    
    To verify this, I had to tweak the registration.dat
    contents to use NAD+PO instead of NAD+GP.

--- src/main/java/uk/nhs/digital/nhsconnect/lab/results/translator/mapper/RequesterMapper.java
+++ src/main/java/uk/nhs/digital/nhsconnect/lab/results/translator/mapper/RequesterMapper.java
@@ -1,14 +1,27 @@
 package uk.nhs.digital.nhsconnect.lab.results.translator.mapper;
 
+import java.util.Optional;
 import org.hl7.fhir.dstu3.model.Practitioner;
 import org.springframework.stereotype.Component;
 import uk.nhs.digital.nhsconnect.lab.results.model.edifact.Message;
-
-import java.util.Optional;
+import uk.nhs.digital.nhsconnect.lab.results.model.edifact.RequesterNameAndAddress;
 
 @Component
 public class RequesterMapper {
+    protected static final String SDS_USER_SYSTEM = "https://fhir.nhs.uk/Id/sds-user-id";
+
     public Optional<Practitioner> map(final Message message) {
-        return Optional.empty();
+        return message.getRequesterNameAndAddress()
+            .map(this::toPractitioner);
+    }
+
+    private Practitioner toPractitioner(final RequesterNameAndAddress requester) {
+        final var result = new Practitioner();
+        result.addIdentifier()
+            .setValue(requester.getIdentifier())
+            .setSystem(SDS_USER_SYSTEM);
+        result.addName()
+            .setText(requester.getRequesterName());
+        return result;
     }
 }
--- src/test/java/uk/nhs/digital/nhsconnect/lab/results/translator/mapper/RequesterMapperTest.java
+++ src/test/java/uk/nhs/digital/nhsconnect/lab/results/translator/mapper/RequesterMapperTest.java
@@ -1,17 +1,64 @@
 package uk.nhs.digital.nhsconnect.lab.results.translator.mapper;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import org.hl7.fhir.dstu3.model.HumanName;
+import org.hl7.fhir.dstu3.model.Practitioner;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import uk.nhs.digital.nhsconnect.lab.results.model.edifact.Message;
+import uk.nhs.digital.nhsconnect.lab.results.model.edifact.RequesterNameAndAddress;
 
-import java.util.ArrayList;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+@ExtendWith(MockitoExtension.class)
 class RequesterMapperTest {
 
+    @Mock
+    private Message message;
+
+    @Mock
+    private RequesterNameAndAddress requester;
+
+    private RequesterMapper mapper;
+
+    @BeforeEach
+    void setUp() {
+        mapper = new RequesterMapper();
+    }
+
     @Test
-    void testMapMessageToPractitioner() {
-        final Message message = new Message(new ArrayList<>());
-        assertTrue(new RequesterMapper().map(message).isEmpty());
+    void testMapMessageToPractitionerNoRequester() {
+        when(message.getRequesterNameAndAddress()).thenReturn(Optional.empty());
+
+        assertThat(mapper.map(message)).isEmpty();
+    }
+
+    @Test
+    void testMapMessageToPractitionerWithRequester() {
+        when(message.getRequesterNameAndAddress()).thenReturn(Optional.of(requester));
+        when(requester.getRequesterName()).thenReturn("Alan Turing");
+        when(requester.getIdentifier()).thenReturn("Identifier");
+
+        Optional<Practitioner> result = mapper.map(message);
+        assertThat(result).isNotEmpty();
+
+        Practitioner practitioner = result.get();
+        assertThat(practitioner.getName())
+            .hasSize(1)
+            .first()
+            .extracting(HumanName::getText)
+            .isEqualTo("Alan Turing");
+        assertThat(practitioner.getIdentifier())
+            .hasSize(1)
+            .first()
+            .satisfies(identifier -> assertAll(
+                () -> assertThat(identifier.getValue()).isEqualTo("Identifier"),
+                () -> assertThat(identifier.getSystem()).isEqualTo(RequesterMapper.SDS_USER_SYSTEM)
+            ));
     }
 }
