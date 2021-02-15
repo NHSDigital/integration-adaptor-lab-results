a76f094279dd0a13b3011eb9e099adf891ad5b95
Matthew O'Reilly <matthewdoreilly@users.noreply.github.com>
2021-02-15 16:53:22 +0000

    NIAD-965: Create pathology record, pathology record mapper and bundle mapper for message translator component. (#35)

--- src/main/java/uk/nhs/digital/nhsconnect/lab/results/translator/mapper/DiagnosticReportMapper.java
+++ src/main/java/uk/nhs/digital/nhsconnect/lab/results/translator/mapper/DiagnosticReportMapper.java
@@ -4,11 +4,9 @@ import org.hl7.fhir.dstu3.model.DiagnosticReport;
 import org.springframework.stereotype.Component;
 import uk.nhs.digital.nhsconnect.lab.results.model.edifact.Message;
 
-import java.util.Optional;
-
 @Component
-public class TestReportMapper {
-    public Optional<DiagnosticReport> map(final Message message) {
-        return Optional.empty();
+public class DiagnosticReportMapper {
+    public DiagnosticReport map(final Message message) {
+        return new DiagnosticReport();
     }
 }
---
+++ src/test/java/uk/nhs/digital/nhsconnect/lab/results/fixtures/FhirFixtures.java
@@ -0,0 +1,53 @@
+package uk.nhs.digital.nhsconnect.lab.results.fixtures;
+
+import org.hl7.fhir.dstu3.model.Bundle;
+import org.hl7.fhir.dstu3.model.Enumerations.AdministrativeGender;
+import org.hl7.fhir.dstu3.model.Identifier;
+import org.hl7.fhir.dstu3.model.InstantType;
+import org.hl7.fhir.dstu3.model.Meta;
+import org.hl7.fhir.dstu3.model.Practitioner;
+import org.hl7.fhir.dstu3.model.UriType;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+@SuppressWarnings("checkstyle:hideutilityclassconstructor")
+public final class FhirFixtures {
+
+    private static final int YEAR = 2010;
+    private static final int DAY_OF_MONTH = 24;
+    private static final int HOUR_OF_DAY = 15;
+    private static final int MINUTE = 41;
+
+    public static Practitioner generateRequester(final String name, final AdministrativeGender gender) {
+        Practitioner requester = new Practitioner();
+
+        requester.addName().setText(name);
+        requester.setGender(gender);
+
+        return requester;
+    }
+
+    public static Bundle generateBundle() {
+        Bundle bundle = new Bundle();
+
+        bundle.setMeta(new Meta()
+                .setLastUpdatedElement(new InstantType(
+                    new GregorianCalendar(YEAR, Calendar.FEBRUARY, DAY_OF_MONTH, HOUR_OF_DAY, MINUTE)
+                ))
+                .setProfile(List.of(new UriType("https://some-profile")))
+        );
+        bundle.setIdentifier(new Identifier()
+                .setSystem("https://some-system")
+                .setValue("some-value-uuid")
+        );
+        bundle.setType(Bundle.BundleType.MESSAGE);
+
+        bundle.addEntry()
+                .setFullUrl("urn:uuid:some-entry-uuid")
+                .setResource(new Practitioner());
+
+        return bundle;
+    }
+}
