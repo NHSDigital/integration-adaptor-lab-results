5b7f28939c33dc377e4f3bb2a74d850a71ea580f
Sailesh Ashokbhai Patel <s.patel@kainos.com>
2021-02-24 10:14:01 +0000

    NIAD-969: refactor PractitionerMapper to use map in chain

--- src/main/java/uk/nhs/digital/nhsconnect/lab/results/translator/mapper/PractitionerMapper.java
+++ src/main/java/uk/nhs/digital/nhsconnect/lab/results/translator/mapper/PractitionerMapper.java
@@ -3,8 +3,6 @@ package uk.nhs.digital.nhsconnect.lab.results.translator.mapper;
 import org.hl7.fhir.dstu3.model.Practitioner;
 import org.springframework.stereotype.Component;
 import uk.nhs.digital.nhsconnect.lab.results.model.edifact.Message;
-import uk.nhs.digital.nhsconnect.lab.results.model.edifact.PerformerNameAndAddress;
-import uk.nhs.digital.nhsconnect.lab.results.model.edifact.RequesterNameAndAddress;
 import uk.nhs.digital.nhsconnect.lab.results.model.edifact.segmentgroup.InvolvedParty;
 
 import java.util.Optional;
@@ -14,35 +12,21 @@ public class PractitionerMapper {
     private static final String SDS_USER_SYSTEM = "https://fhir.nhs.uk/Id/sds-user-id";
 
     public Optional<Practitioner> mapRequester(final Message message) {
-        Optional<RequesterNameAndAddress> requester = message.getInvolvedParties().stream()
+        return message.getInvolvedParties().stream()
             .map(InvolvedParty::getRequesterNameAndAddress)
             .flatMap(Optional::stream)
+            .map(r -> mapToPractitioner(r.getIdentifier(), r.getRequesterName()))
             .findAny();
-
-        if (requester.isPresent()) {
-            final var r = requester.get();
-            Practitioner practitioner = mapToPractitioner(r.getIdentifier(), r.getRequesterName());
-            return Optional.of(practitioner);
-        } else {
-            return Optional.empty();
-        }
     }
 
 
     public Optional<Practitioner> mapPerformer(final Message message) {
-        Optional<PerformerNameAndAddress> performer = message.getInvolvedParties().stream()
+        return message.getInvolvedParties().stream()
             .map(InvolvedParty::getPerformerNameAndAddress)
             .flatMap(Optional::stream)
             .filter(p -> !p.getIdentifier().isBlank())
+            .map(p -> mapToPractitioner(p.getIdentifier(), p.getPerformerName()))
             .findAny();
-
-        if (performer.isPresent()) {
-            final var p = performer.get();
-            Practitioner practitioner = mapToPractitioner(p.getIdentifier(), p.getPerformerName());
-            return Optional.of(practitioner);
-        } else {
-            return Optional.empty();
-        }
     }
 
     private Practitioner mapToPractitioner(final String identifier, final String name) {
