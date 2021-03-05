f722d8f75f95c99c12f08d67c1e1b1e3ab45b87d
Sailesh Ashokbhai Patel <s.patel@kainos.com>
2021-03-05 10:09:04 +0000

    NIAD-971: add subject to DiagnosticReportMapper

--- src/main/java/uk/nhs/digital/nhsconnect/lab/results/translator/mapper/DiagnosticReportMapper.java
+++ src/main/java/uk/nhs/digital/nhsconnect/lab/results/translator/mapper/DiagnosticReportMapper.java
@@ -5,6 +5,7 @@ import org.hl7.fhir.dstu3.model.CodeableConcept;
 import org.hl7.fhir.dstu3.model.Coding;
 import org.hl7.fhir.dstu3.model.DiagnosticReport;
 import org.hl7.fhir.dstu3.model.Identifier;
+import org.hl7.fhir.dstu3.model.Patient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import uk.nhs.digital.nhsconnect.lab.results.model.edifact.DiagnosticReportDateIssued;
@@ -12,6 +13,7 @@ import uk.nhs.digital.nhsconnect.lab.results.model.edifact.Message;
 import uk.nhs.digital.nhsconnect.lab.results.model.edifact.Reference;
 import uk.nhs.digital.nhsconnect.lab.results.model.edifact.ReportStatusCode;
 import uk.nhs.digital.nhsconnect.lab.results.model.edifact.segmentgroup.ServiceReportDetails;
+import uk.nhs.digital.nhsconnect.lab.results.utils.ResourceFullUrlGenerator;
 import uk.nhs.digital.nhsconnect.lab.results.utils.TimestampService;
 import uk.nhs.digital.nhsconnect.lab.results.utils.UUIDGenerator;
 
@@ -25,51 +27,55 @@ import java.util.Map;
 public class DiagnosticReportMapper {
 
     private final UUIDGenerator uuidGenerator;
+    private final ResourceFullUrlGenerator fullUrlGenerator;
     private final Map<ReportStatusCode, DiagnosticReport.DiagnosticReportStatus> statusMap = Map.of(
         ReportStatusCode.UNSPECIFIED, DiagnosticReport.DiagnosticReportStatus.UNKNOWN
     );
 
-    public DiagnosticReport map(final Message message) {
-        DiagnosticReport report = new DiagnosticReport();
+    public DiagnosticReport map(final Message message, Patient patient) {
+        DiagnosticReport fhir = new DiagnosticReport();
+        fhir.setId(uuidGenerator.generateUUID());
+
         ServiceReportDetails serviceReportDetails = message.getServiceReportDetails();
 
-        report.setId(uuidGenerator.generateUUID());
-
-        mapDateToLong(serviceReportDetails.getDateIssued(), report);
-        report.setStatus(statusMap.get(serviceReportDetails.getStatus().getEvent()));
-        mapIdentifier(serviceReportDetails.getReference(), report);
-
+        // fhir.issued = SG2.DTM
+        mapDateToLong(serviceReportDetails.getDateIssued(), fhir);
+        // fhir.status
+        fhir.setStatus(statusMap.get(serviceReportDetails.getStatus().getEvent()));
+        // fhir.identifier
+        mapIdentifier(serviceReportDetails.getReference(), fhir);
         Coding coding = new Coding();
         coding.setCode("721981007");
         coding.setSystem("http://snomed.info/sct");
         coding.setDisplay("Diagnostic studies report");
-
-        report.setCode(new CodeableConcept().setCoding(List.of(coding)));
+        // fhir.code
+        fhir.setCode(new CodeableConcept().setCoding(List.of(coding)));
+        // fhir.subject
+        fhir.getSubject().setReference(fullUrlGenerator.generate(patient));
 
         /*
             TODO: Add the following
-                - BasedOn
-                - Subject
-                - Performer
-                - Specimen
-                - Result
+                - BasedOn - ProcedureReport
+                - Performer - Practitioner/Organization
+                - Specimen - could be a list?
+                - Result - Observation
          */
 
-        return report;
+        return fhir;
     }
 
-    private void mapDateToLong(final DiagnosticReportDateIssued reportIssuedDate, final DiagnosticReport diagnosticReport) {
+    private void mapDateToLong(final DiagnosticReportDateIssued reportIssuedDate, final DiagnosticReport fhir) {
         LocalDateTime dateIssued = reportIssuedDate.getDateIssued();
         ZonedDateTime zonedDateTime = dateIssued.atZone(TimestampService.UK_ZONE);
 
-        diagnosticReport.getIssued().setTime(zonedDateTime.toInstant().toEpochMilli());
+        fhir.getIssued().setTime(zonedDateTime.toInstant().toEpochMilli());
     }
 
-    private void mapIdentifier(final Reference reference, final DiagnosticReport diagnosticReport) {
+    private void mapIdentifier(final Reference reference, final DiagnosticReport fhir) {
         final Identifier identifier = new Identifier();
 
         identifier.setValue(reference.getNumber());
 
-        diagnosticReport.addIdentifier(identifier);
+        fhir.addIdentifier(identifier);
     }
 }
