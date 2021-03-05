aa31bf995ccca84ba1f95a153806c310f20025e8
Andrew Walker <andrewwa@kainos.com>
2021-03-05 10:39:21 +0000

    Map test results
    This commit takes all of the segment group 18s (denoted
    by `GIS+N`: class `LabResult`) and maps them as test
    results – "Observations" in FHIR.
    
    This is not correct, as some edifact `GIS+N` groups
    actually refer to test groups, with slightly different
    mappings to FHIR. Test groups will be supported in a
    future commit.

--- src/main/java/uk/nhs/digital/nhsconnect/lab/results/translator/mapper/ObservationMapper.java
+++ src/main/java/uk/nhs/digital/nhsconnect/lab/results/translator/mapper/ObservationMapper.java
@@ -1,18 +1,126 @@
 package uk.nhs.digital.nhsconnect.lab.results.translator.mapper;
 
+import org.apache.commons.lang3.StringUtils;
 import org.hl7.fhir.dstu3.model.Observation;
+import org.hl7.fhir.dstu3.model.Observation.ObservationReferenceRangeComponent;
+import org.hl7.fhir.dstu3.model.Observation.ObservationStatus;
+import org.hl7.fhir.dstu3.model.Quantity;
+import org.hl7.fhir.dstu3.model.Quantity.QuantityComparator;
+import org.hl7.fhir.dstu3.model.SimpleQuantity;
 import org.springframework.stereotype.Component;
+import uk.nhs.digital.nhsconnect.lab.results.model.edifact.FreeTextSegment;
+import uk.nhs.digital.nhsconnect.lab.results.model.edifact.MeasurementValueComparator;
 import uk.nhs.digital.nhsconnect.lab.results.model.edifact.Message;
+import uk.nhs.digital.nhsconnect.lab.results.model.edifact.TestStatus;
+import uk.nhs.digital.nhsconnect.lab.results.model.edifact.TestStatusCode;
+import uk.nhs.digital.nhsconnect.lab.results.model.edifact.segmentgroup.InvestigationSubject;
+import uk.nhs.digital.nhsconnect.lab.results.model.edifact.segmentgroup.LabResult;
+import uk.nhs.digital.nhsconnect.lab.results.model.edifact.segmentgroup.ResultReferenceRange;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
 public class ObservationMapper {
-    public List<Observation> mapToTestGroups(final Message message) {
-        return List.of(new Observation());
+    public List<Observation> mapToTestGroupsAndResults(final Message message) {
+        // test groups are GIS+N blocks with SEQ segments
+        // they should have RFF+ASL:X where X is the SEQ value of the S16
+
+        // test results are GIS+N blocks without SEQ segments
+        // they should have RFF+ARL:Y where Y is the SEQ value of the test group
+
+        final InvestigationSubject subject = message.getServiceReportDetails().getSubject();
+        final List<LabResult> labResults = subject.getLabResults();
+
+        // start by assuming everything is a test result
+        return labResults.stream().map(labResult -> {
+            final var result = new Observation();
+
+            mapStatus(labResult, result);
+            mapValueQuantity(labResult, result);
+            mapCode(labResult, result);
+            mapReferenceRange(labResult, result);
+            mapComment(labResult, result);
+
+            return result;
+        })
+            .collect(Collectors.toList());
     }
 
-    public List<Observation> mapToTestResults(final Message message) {
-        return List.of(new Observation());
+    private void mapStatus(LabResult labResult, Observation result) {
+        // Observation.status = SG18.STS.C555.9011
+        labResult.getTestStatus()
+            .map(TestStatus::getTestStatusCode)
+            .map(TestStatusCode::getDescription)
+            .map(String::toLowerCase)
+            .map(ObservationStatus::fromCode)
+            .ifPresent(result::setStatus);
+    }
+
+    private void mapValueQuantity(LabResult labResult, Observation result) {
+        // Observation.value.valueQuantity.*
+        labResult.getInvestigationResult().ifPresent(investigationResult -> {
+            final var quantity = new Quantity();
+
+            // Observation.value.valueQuantity.value = SG18.RSL.C830(1).6314
+            quantity.setValue(investigationResult.getMeasurementValue());
+
+            // Observation.value.valueQuantity.unit = SG18.RSL.C848.6410
+            quantity.setUnit(investigationResult.getMeasurementUnit());
+
+            // Observation.value.valueQuantity.comparator = SG18.RSL.C830(1).6321
+            investigationResult.getMeasurementValueComparator()
+                .map(MeasurementValueComparator::getDescription)
+                .map(QuantityComparator::fromCode)
+                .ifPresent(quantity::setComparator);
+
+            result.setValue(quantity);
+        });
+    }
+
+    private void mapCode(LabResult labResult, Observation result) {
+        // Observation.code = SG18.INV.C847.9930 and SG18.INV.C847.9931
+        result.getCode().setText(labResult.getInvestigation().getInvestigationDescription());
+        result.getCode().addCoding().setCode(labResult.getInvestigation().getInvestigationCode());
+    }
+
+    private void mapReferenceRange(LabResult labResult, Observation result) {
+        // Observation.referenceRange.*
+        labResult.getResultReferenceRanges().stream()
+            .map(ResultReferenceRange::getDetails)
+            .map(rangeDetail -> {
+                final var range = new ObservationReferenceRangeComponent();
+
+                // Observation.referenceRange.low = SG20.RND.6162
+                rangeDetail.getLowerLimit()
+                    .map(this::toSimpleQuantity)
+                    .ifPresent(range::setLow);
+
+                // Observation.referenceRange.high = SG20.RND.6152
+                rangeDetail.getUpperLimit()
+                    .map(this::toSimpleQuantity)
+                    .ifPresent(range::setHigh);
+
+                return range;
+            })
+            .forEach(result::addReferenceRange);
+    }
+
+    private void mapComment(LabResult labResult, Observation result) {
+        // Observation.comment = SG18.FTX.C108.4440(1-5)
+        Optional.of(labResult.getFreeTexts().stream()
+            .map(FreeTextSegment::getTexts)
+            .map(texts -> String.join(" ", texts))
+            .collect(Collectors.joining("\n")))
+            .filter(StringUtils::isNotBlank)
+            .ifPresent(result::setComment);
+    }
+
+    private SimpleQuantity toSimpleQuantity(final BigDecimal value) {
+        final var simpleQuantity = new SimpleQuantity();
+        simpleQuantity.setValue(value);
+        return simpleQuantity;
     }
 }
--- src/test/java/uk/nhs/digital/nhsconnect/lab/results/translator/mapper/ObservationMapperTest.java
+++ src/test/java/uk/nhs/digital/nhsconnect/lab/results/translator/mapper/ObservationMapperTest.java
@@ -1,25 +1,210 @@
 package uk.nhs.digital.nhsconnect.lab.results.translator.mapper;
 
+import org.hl7.fhir.dstu3.model.Coding;
+import org.hl7.fhir.dstu3.model.Observation;
+import org.hl7.fhir.dstu3.model.Observation.ObservationReferenceRangeComponent;
+import org.hl7.fhir.dstu3.model.Observation.ObservationStatus;
+import org.hl7.fhir.dstu3.model.Quantity.QuantityComparator;
+import org.hl7.fhir.dstu3.model.SimpleQuantity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import uk.nhs.digital.nhsconnect.lab.results.model.edifact.Message;
+import uk.nhs.digital.nhsconnect.lab.results.model.edifact.message.MissingSegmentException;
 
-import java.util.ArrayList;
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
+@SuppressWarnings("checkstyle:MagicNumber")
 class ObservationMapperTest {
+    private ObservationMapper mapper;
 
-    @Test
-    void testMapTestGroups() {
-        final Message message = new Message(new ArrayList<>());
-
-        assertFalse(new ObservationMapper().mapToTestGroups(message).isEmpty());
+    @BeforeEach
+    void setUp() {
+        mapper = new ObservationMapper();
     }
 
     @Test
-    void testMapTestResults() {
-        final Message message = new Message(new ArrayList<>());
+    void testMapToObservationsNonePresent() {
+        final Message message = new Message(Collections.emptyList());
 
-        assertFalse(new ObservationMapper().mapToTestResults(message).isEmpty());
+        final var observations = mapper.mapToTestGroupsAndResults(message);
+
+        assertThat(observations).isEmpty();
+    }
+
+    @Test
+    void testMapToObservationsMissingRequiredSegments() {
+        final Message message = new Message(List.of(
+            "S02+02", // ServiceReportDetails
+            "S06+06", // InvestigationSubject
+            "GIS+N" // LabResult
+        ));
+
+        assertThatThrownBy(() -> mapper.mapToTestGroupsAndResults(message))
+            .isExactlyInstanceOf(MissingSegmentException.class)
+            .hasMessageStartingWith("EDIFACT section is missing segment");
+    }
+
+    @Test
+    void testMapToObservationsOnlyRequiredSegmentLaboratoryInvestigation() {
+        final Message message = new Message(List.of(
+            "S02+02", // ServiceReportDetails
+            "S06+06", // InvestigationSubject
+            "GIS+N", // LabResult
+            "INV+MQ+code:911::description", // LaboratoryInvestigation
+            "GIS+N", // LabResult
+            "INV+MQ+code:911::description" // LaboratoryInvestigation
+        ));
+
+        final var observations = mapper.mapToTestGroupsAndResults(message);
+
+        assertThat(observations).hasSize(2)
+            .allSatisfy(observation -> assertThat(observation.getCode()).satisfies(codeConcept -> assertAll(
+                () -> assertThat(codeConcept.getText()).isEqualTo("description"),
+                () -> assertThat(codeConcept.getCoding()).hasSize(1).first()
+                    .extracting(Coding::getCode)
+                    .isEqualTo("code")
+            )));
+    }
+
+    @Test
+    void testMapToTestGroupsAndResultsLaboratoryInvestigationResult() {
+        final Message message = new Message(List.of(
+            "S02+02", // ServiceReportDetails
+            "S06+06", // InvestigationSubject
+            "GIS+N", // LabResult
+            "INV+MQ+c:911::d", // LaboratoryInvestigation
+
+            "RSL+NV+1.23:7++:::units" // LaboratoryInvestigationResult
+        ));
+
+        final var observations = mapper.mapToTestGroupsAndResults(message);
+
+        assertThat(observations).hasSize(1)
+            .first()
+            .extracting(Observation::getValueQuantity)
+            .isNotNull()
+            .satisfies(quantity -> assertAll(
+                () -> assertThat(quantity.getValue()).isEqualTo("1.23"),
+                () -> assertThat(quantity.getComparator()).isEqualTo(QuantityComparator.LESS_THAN),
+                () -> assertThat(quantity.getUnit()).isEqualTo("units")
+            ));
+    }
+
+    @Test
+    void testMapToTestGroupsAndResultsTestStatusCode() {
+        final Message message = new Message(List.of(
+            "S02+02", // ServiceReportDetails
+            "S06+06", // InvestigationSubject
+            "GIS+N", // LabResult
+            "INV+MQ+c:911::d", // LaboratoryInvestigation
+
+            "STS++PR" // TestStatus
+        ));
+
+        final var observations = mapper.mapToTestGroupsAndResults(message);
+
+        assertThat(observations).hasSize(1)
+            .first()
+            .extracting(Observation::getStatus)
+            .isEqualTo(ObservationStatus.PRELIMINARY);
+    }
+
+    @Test
+    void testMapToTestGroupsAndResultsFreeTexts() {
+        final Message message = new Message(List.of(
+            "S02+02", // ServiceReportDetails
+            "S06+06", // InvestigationSubject
+            "GIS+N", // LabResult
+            "INV+MQ+c:911::d", // LaboratoryInvestigation
+
+            "FTX+RIT+++multi:line", // FreeTextSegment
+            "FTX+RIT+++comment"
+        ));
+
+        final var observations = mapper.mapToTestGroupsAndResults(message);
+
+        assertThat(observations).hasSize(1)
+            .first()
+            .extracting(Observation::getComment)
+            .isEqualTo("multi line\ncomment");
+    }
+
+    @Test
+    void testMapToTestGroupsAndResultsRange() {
+        final Message message = new Message(List.of(
+            "S02+02", // ServiceReportDetails
+            "S06+06", // InvestigationSubject
+            "GIS+N", // LabResult
+            "INV+MQ+c:911::d", // LaboratoryInvestigation
+
+            "S20+20", // ResultReferenceRange
+            "RND+U+-1.0+1.0" // RangeDetail
+        ));
+
+        final var observations = mapper.mapToTestGroupsAndResults(message);
+
+        final var assertReferenceRange = assertThat(observations).hasSize(1)
+            .first()
+            .extracting(Observation::getReferenceRangeFirstRep);
+        assertAll(
+            () -> assertReferenceRange.extracting(ObservationReferenceRangeComponent::getLow)
+                .extracting(SimpleQuantity::getValue)
+                .extracting(BigDecimal::intValue)
+                .isEqualTo(-1),
+            () -> assertReferenceRange.extracting(ObservationReferenceRangeComponent::getHigh)
+                .extracting(SimpleQuantity::getValue)
+                .extracting(BigDecimal::intValue)
+                .isEqualTo(1)
+        );
+    }
+
+    @Test
+    void testCanMapMultipleTestResults() {
+        final Message message = new Message(List.of(
+            "S02+02",
+            "S06+06",
+            "GIS+N",
+            "INV+MQ+c:911::d",
+            "GIS+N",
+            "INV+MQ+c:911::d",
+            "GIS+N",
+            "INV+MQ+c:911::d"
+        ));
+
+        final List<Observation> actual = mapper.mapToTestGroupsAndResults(message);
+
+        assertThat(actual).hasSize(3);
+    }
+
+    @Test
+    @Disabled
+    void testCanMapTestGroupWithOneResult() {
+        final Message message = new Message(List.of(
+            "S02+02",
+            "S06+06",
+            "GIS+N",
+            "INV+MQ+44O..:911::Test group",
+            "SEQ++1",
+            "FTX+SPC+++This is a test group",
+            "RFF+ASL:1",
+            "GIS+N",
+            "INV+MQ+:::Test result",
+            "FTX+RIT+++This is a test result:belonging to the test group",
+            "RFF+ARL:1"
+        ));
+
+        final List<Observation> actual = mapper.mapToTestGroupsAndResults(message);
+
+        final var assertObservations = assertThat(actual).hasSize(2);
+        assertAll(
+            () -> assertObservations.first()
+        );
     }
 }
