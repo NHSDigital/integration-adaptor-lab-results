acc1f78a7cae676f68ecc4698b526c930f370b6c
Sailesh Ashokbhai Patel <s.patel@kainos.com>
2021-03-10 14:39:06 +0000

    NIAD-971: create internal mapper class to hold DiagnosticReport state
    - Created private class inside DiagnosticReportMapper to hold the state of resources

--- src/main/java/uk/nhs/digital/nhsconnect/lab/results/translator/mapper/DiagnosticReportMapper.java
+++ src/main/java/uk/nhs/digital/nhsconnect/lab/results/translator/mapper/DiagnosticReportMapper.java
@@ -41,67 +41,84 @@ public class DiagnosticReportMapper {
     public DiagnosticReport mapToDiagnosticReport(final Message message, Patient patient, List<Specimen> specimens,
                                                   List<Observation> observations, Practitioner performingPractitioner,
                                                   Organization performingOrganization) {
-        DiagnosticReport fhir = new DiagnosticReport();
-        fhir.setId(uuidGenerator.generateUUID());
 
-        ServiceReportDetails serviceReportDetails = message.getServiceReportDetails();
-
-        // fhir.issued = SG2.DTM
-        mapIssued(serviceReportDetails.getDateIssued(), fhir);
-        // fhir.status
-        fhir.setStatus(STATUS_MAP.get(serviceReportDetails.getStatus().getEvent()));
-        // fhir.identifier
-        mapIdentifier(serviceReportDetails.getReference(), fhir);
-        // fhir.code
-        mapCode(fhir);
-        // fhir.subject
-        fhir.getSubject().setReference(fullUrlGenerator.generate(patient));
-        // fhir.specimens
-        mapSpecimens(specimens, fhir);
-        // fhir.results
-        mapObservations(observations, fhir);
-        // fhir.performer
-        mapPerformer(performingPractitioner, performingOrganization, fhir);
-        /* TODO: Add BasedOn - ProcedureReport & Result - Observation */
-        return fhir;
+        return new InternalMapper(message, patient, specimens, observations, performingPractitioner,
+            performingOrganization).map();
     }
 
-    private void mapIssued(final DiagnosticReportDateIssued reportIssuedDate, final DiagnosticReport fhir) {
-        LocalDateTime dateIssued = reportIssuedDate.getDateIssued();
-        ZonedDateTime zonedDateTime = dateIssued.atZone(TimestampService.UK_ZONE);
-        fhir.setIssued(Date.from(zonedDateTime.toInstant()));
-    }
+    @RequiredArgsConstructor
+    private class InternalMapper {
+        private final Message message;
+        private final Patient patient;
+        private final List<Specimen> specimens;
+        private final List<Observation> observations;
+        private final Practitioner performingPractitioner;
+        private final Organization performingOrganization;
+        private DiagnosticReport fhir;
+        private ServiceReportDetails serviceReportDetails;
 
-    private void mapIdentifier(final Reference reference, final DiagnosticReport fhir) {
-        final Identifier identifier = new Identifier();
-        identifier.setValue(reference.getNumber());
-        fhir.addIdentifier(identifier);
-    }
+        public DiagnosticReport map() {
+            this.fhir = new DiagnosticReport();
+            this.serviceReportDetails = message.getServiceReportDetails();
 
-    private void mapSpecimens(final List<Specimen> specimens, final DiagnosticReport fhir) {
-        specimens.stream()
-            .map(fullUrlGenerator::generate)
-            .forEach(reference -> fhir.addSpecimen().setReference(reference));
-    }
+            fhir.setId(uuidGenerator.generateUUID());
+            // fhir.issued = SG2.DTM
+            mapIssued(serviceReportDetails.getDateIssued());
+            // fhir.status
+            fhir.setStatus(STATUS_MAP.get(serviceReportDetails.getStatus().getEvent()));
+            // fhir.identifier
+            mapIdentifier(serviceReportDetails.getReference());
+            // fhir.code
+            mapCode();
+            // fhir.subject
+            fhir.getSubject().setReference(fullUrlGenerator.generate(patient));
+            // fhir.specimens
+            mapSpecimens(specimens);
+            // fhir.results
+            mapObservations(observations);
+            // fhir.performer
+            mapPerformer(performingPractitioner, performingOrganization);
+            /* TODO: Add BasedOn - ProcedureReport & Result - Observation */
 
-    private void mapCode(final DiagnosticReport fhir) {
-        fhir.getCode().addCoding()
-            .setDisplay(CODE_DISPLAY)
-            .setCode(CODE_NUMBER)
-            .setSystem(CODE_SYSTEM);
-    }
+            return fhir;
+        }
 
-    private void mapPerformer(Practitioner performingPractitioner, Organization performingOrganization,
-                              final DiagnosticReport fhir) {
-        Optional.ofNullable(performingOrganization).ifPresent(organization ->
-            fhir.addPerformer().getActor().setReference(fullUrlGenerator.generate(organization)));
-        Optional.ofNullable(performingPractitioner).ifPresent(performer ->
-            fhir.addPerformer().getActor().setReference(fullUrlGenerator.generate(performer)));
-    }
+        private void mapIssued(final DiagnosticReportDateIssued reportIssuedDate) {
+            LocalDateTime dateIssued = reportIssuedDate.getDateIssued();
+            ZonedDateTime zonedDateTime = dateIssued.atZone(TimestampService.UK_ZONE);
+            fhir.setIssued(Date.from(zonedDateTime.toInstant()));
+        }
 
-    private void mapObservations(final List<Observation> observations, final DiagnosticReport fhir) {
-        observations.stream()
-            .map(fullUrlGenerator::generate)
-            .forEach(reference -> fhir.addResult().setReference(reference));
+        private void mapIdentifier(final Reference reference) {
+            final Identifier identifier = new Identifier();
+            identifier.setValue(reference.getNumber());
+            fhir.addIdentifier(identifier);
+        }
+
+        private void mapSpecimens(final List<Specimen> specimens) {
+            specimens.stream()
+                .map(fullUrlGenerator::generate)
+                .forEach(reference -> fhir.addSpecimen().setReference(reference));
+        }
+
+        private void mapCode() {
+            fhir.getCode().addCoding()
+                .setDisplay(CODE_DISPLAY)
+                .setCode(CODE_NUMBER)
+                .setSystem(CODE_SYSTEM);
+        }
+
+        private void mapPerformer(Practitioner performingPractitioner, Organization performingOrganization) {
+            Optional.ofNullable(performingOrganization).ifPresent(organization ->
+                fhir.addPerformer().getActor().setReference(fullUrlGenerator.generate(organization)));
+            Optional.ofNullable(performingPractitioner).ifPresent(performer ->
+                fhir.addPerformer().getActor().setReference(fullUrlGenerator.generate(performer)));
+        }
+
+        private void mapObservations(final List<Observation> observations) {
+            observations.stream()
+                .map(fullUrlGenerator::generate)
+                .forEach(reference -> fhir.addResult().setReference(reference));
+        }
     }
 }
