5abbeb51da561251e48dc73b34a738926d806b0d
Bartek Sarul <b.sarul@kainos.com>
2021-06-08 13:09:49 +0200

    https://github.com/nhsconnect/integration-adaptor-lab-results/issues/152

--- src/main/java/uk/nhs/digital/nhsconnect/lab/results/translator/mapper/ObservationMapper.java
+++ src/main/java/uk/nhs/digital/nhsconnect/lab/results/translator/mapper/ObservationMapper.java
@@ -225,7 +225,9 @@ public class ObservationMapper {
             // Observation.code = SG18.INV.C847.9930 and SG18.INV.C847.9931
             final var coding = observation.getCode().addCoding();
             labResult.getInvestigation().getCode().ifPresent(coding::setCode);
-            labResult.getInvestigation().getDescription().ifPresent(coding::setDisplay);
+            labResult.getInvestigation().getDescription()
+                .map(MappingUtils::unescape)
+                .ifPresent(coding::setDisplay);
             labResult.getInvestigation().getCodingType()
                 .map(this::getSystemValue)
                 .ifPresent(coding::setSystem);
