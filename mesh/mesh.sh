289da88388ef48a1d2953de00279b8426c6bb203
Alex-Nita <alexandru.nita@madetech.com>
2025-03-21 15:27:33 +0000

    Updated Test scrips for better visualisation of the requests being sent and the responses.

--- mesh/mesh.sh
+++ mesh/mesh.sh
@@ -9,6 +9,34 @@ MESH_ENDPOINT_CERT="${MESH_ENDPOINT_CERT}"
 CURL_FLAGS="${CURL_FLAGS:--k}" # insecure, disable cert validation for fake-mesh. Add -i for headers
 WORKFLOW_ID="${WORKFLOW_ID:PATH_MEDRPT_V3}"
 
+print_symbols() {
+  local x="$1"
+
+  local symbols_line=""
+  for ((i = 0; i < x; i++)); do
+    symbols_line+="="
+  done
+
+  printf "$symbols_line"
+}
+
+output_console(){
+  local title="$1"
+  local length=${#title}
+
+  print_symbols 20
+  echo -n "$1"
+  print_symbols 20
+
+  printf "\n$2\n"
+
+  print_symbols 20
+  print_symbols length
+  print_symbols 20
+
+  printf "\n"
+}
+
 create_token() {
   local nonce
   local nonce_count
@@ -22,49 +50,76 @@ create_token() {
   hash_content="${MAILBOX_ID}:${nonce}:${nonce_count}:${MAILBOX_PASSWORD}:${timestamp}"
   hash_value=$(echo -n "${hash_content}" | openssl dgst -sha256 -hmac "${SHARED_KEY}" | sed 's/^.* //')
   TOKEN="NHSMESH ${MAILBOX_ID}:${nonce}:${nonce_count}:${timestamp}:${hash_value}"
+
+  output_console "Generated Token" "${TOKEN}"
 }
 
 authorization() {
-  curl ${CURL_FLAGS} -X POST "https://${HOST}/messageexchange/${MAILBOX_ID}" \
+
+  output_console "Authorization Request" "curl ${CURL_FLAGS} -X POST \"https://${HOST}/messageexchange/${MAILBOX_ID}\" --cert \"${MESH_ENDPOINT_CERT}\" --key \"${MESH_ENDPOINT_PRIVATE_KEY}\" -H \"Authorization: ${TOKEN}\" -H 'Mex-ClientVersion: 1.0' -H 'Mex-JavaVersion: 1.7.0_60' -H 'Mex-OSArchitecture: Windows 7' -H 'Mex-OSName: x86' -H 'Mex-OSVersion: 6.1'"
+
+  local response=$(curl ${CURL_FLAGS} -X POST "https://${HOST}/messageexchange/${MAILBOX_ID}" \
     --cert "${MESH_ENDPOINT_CERT}" --key "${MESH_ENDPOINT_PRIVATE_KEY}" -H "Authorization: ${TOKEN}" \
     -H 'Mex-ClientVersion: 1.0' -H 'Mex-JavaVersion: 1.7.0_60' -H 'Mex-OSArchitecture: Windows 7' \
-    -H 'Mex-OSName: x86' -H 'Mex-OSVersion: 6.1'
+    -H 'Mex-OSName: x86' -H 'Mex-OSVersion: 6.1')
+
+  output_console "Response" "$response"
 }
 
 inbox() {
-  curl ${CURL_FLAGS} -X GET "https://${HOST}/messageexchange/${MAILBOX_ID}/inbox" \
+
+  output_console "Inbox Request" "curl ${CURL_FLAGS} -X GET \"https://${HOST}/messageexchange/${MAILBOX_ID}/inbox\" --cert \"${MESH_ENDPOINT_CERT}\" --key \"${MESH_ENDPOINT_PRIVATE_KEY}\" -H \"Authorization: ${TOKEN}\" -H 'Mex-ClientVersion: 1.0' -H 'Mex-JavaVersion: 1.7.0_60' -H 'Mex-OSArchitecture: Windows 7' -H 'Mex-OSName: x86' -H 'Mex-OSVersion: 6.1'"
+
+  local response=$(curl ${CURL_FLAGS} -X GET "https://${HOST}/messageexchange/${MAILBOX_ID}/inbox" \
     --cert "${MESH_ENDPOINT_CERT}" --key "${MESH_ENDPOINT_PRIVATE_KEY}" -H "Authorization: ${TOKEN}" \
     -H 'Mex-ClientVersion: 1.0' -H 'Mex-JavaVersion: 1.7.0_60' -H 'Mex-OSArchitecture: Windows 7' \
-    -H 'Mex-OSName: x86' -H 'Mex-OSVersion: 6.1'
+    -H 'Mex-OSName: x86' -H 'Mex-OSVersion: 6.1')
+
+  output_console "Response" "$response"
 }
 
 send() {
   local body
   body="$1"
-  curl ${CURL_FLAGS} -X POST "https://${HOST}/messageexchange/${MAILBOX_ID}/outbox" \
+
+  output_console "Send Request" "curl ${CURL_FLAGS} -X POST \"https://${HOST}/messageexchange/${MAILBOX_ID}/outbox\" --cert \"${MESH_ENDPOINT_CERT}\" --key \"${MESH_ENDPOINT_PRIVATE_KEY}\" -H \"Authorization: ${TOKEN}\" -d \"${body}\" -H \"Mex-From: ${MAILBOX_ID}\" -H \"Mex-To: ${TO_MAILBOX}\" -H \"Mex-WorkflowID: ${WORKFLOW_ID}\" -H 'Content-Type:application/octet-stream' -H 'Mex-MessageType: DATA'  -H 'Mex-FileName: test-filename.txt' -H 'Mex-Version: 1.0' -H 'Mex-ClientVersion: 1.0' -H 'Mex-JavaVersion: 1.7.0_60' -H 'Mex-OSArchitecture: Windows 7' -H 'Mex-OSName: x86' -H 'Mex-OSVersion: 6.1'"
+
+  local response=$(curl ${CURL_FLAGS} -X POST "https://${HOST}/messageexchange/${MAILBOX_ID}/outbox" \
     --cert "${MESH_ENDPOINT_CERT}" --key "${MESH_ENDPOINT_PRIVATE_KEY}" -H "Authorization: ${TOKEN}" -d "${body}" \
     -H "Mex-From: ${MAILBOX_ID}" -H "Mex-To: ${TO_MAILBOX}" -H "Mex-WorkflowID: ${WORKFLOW_ID}" \
     -H 'Content-Type:application/octet-stream' -H 'Mex-MessageType: DATA'  -H 'Mex-FileName: test-filename.txt' \
     -H 'Mex-Version: 1.0' -H 'Mex-ClientVersion: 1.0' -H 'Mex-JavaVersion: 1.7.0_60' -H 'Mex-OSArchitecture: Windows 7' \
-    -H 'Mex-OSName: x86' -H 'Mex-OSVersion: 6.1'
+    -H 'Mex-OSName: x86' -H 'Mex-OSVersion: 6.1')
+
+  output_console "Response" "$response"
 }
 
 download() {
   local message_id
   message_id=$1
-  curl ${CURL_FLAGS} -X GET "https://${HOST}/messageexchange/${MAILBOX_ID}/inbox/${message_id}" \
+
+  output_console "Download Request" "curl ${CURL_FLAGS} -X GET \"https://${HOST}/messageexchange/${MAILBOX_ID}/inbox/${message_id}\" --cert \"${MESH_ENDPOINT_CERT}\" --key \"${MESH_ENDPOINT_PRIVATE_KEY}\" -H \"Authorization: ${TOKEN}\" -H 'Mex-ClientVersion: 1.0' -H 'Mex-JavaVersion: 1.7.0_60' -H 'Mex-OSArchitecture: Windows 7' -H 'Mex-OSName: x86' -H 'Mex-OSVersion: 6.1'"
+
+  local response=$(curl ${CURL_FLAGS} -X GET "https://${HOST}/messageexchange/${MAILBOX_ID}/inbox/${message_id}" \
     --cert "${MESH_ENDPOINT_CERT}" --key "${MESH_ENDPOINT_PRIVATE_KEY}" -H "Authorization: ${TOKEN}" \
     -H 'Mex-ClientVersion: 1.0' -H 'Mex-JavaVersion: 1.7.0_60' -H 'Mex-OSArchitecture: Windows 7' \
-    -H 'Mex-OSName: x86' -H 'Mex-OSVersion: 6.1'
+    -H 'Mex-OSName: x86' -H 'Mex-OSVersion: 6.1')
+
+  output_console "Response" "$response"
 }
 
 ack() {
   local message_id
   message_id=$1
-  curl -i ${CURL_FLAGS} -X PUT "https://${HOST}/messageexchange/${MAILBOX_ID}/inbox/${message_id}/status/acknowledged" \
+
+  output_console "Ack Request" "curl -i ${CURL_FLAGS} -X PUT \"https://${HOST}/messageexchange/${MAILBOX_ID}/inbox/${message_id}/status/acknowledged\" --cert \"${MESH_ENDPOINT_CERT}\" --key \"${MESH_ENDPOINT_PRIVATE_KEY}\" -H \"Authorization: ${TOKEN}\" -H 'Mex-ClientVersion: 1.0' -H 'Mex-JavaVersion: 1.7.0_60' -H 'Mex-OSArchitecture: Windows 7' -H 'Mex-OSName: x86' -H 'Mex-OSVersion: 6.1'"
+
+  local response=$(curl -i ${CURL_FLAGS} -X PUT "https://${HOST}/messageexchange/${MAILBOX_ID}/inbox/${message_id}/status/acknowledged" \
     --cert "${MESH_ENDPOINT_CERT}" --key "${MESH_ENDPOINT_PRIVATE_KEY}" -H "Authorization: ${TOKEN}" \
     -H 'Mex-ClientVersion: 1.0' -H 'Mex-JavaVersion: 1.7.0_60' -H 'Mex-OSArchitecture: Windows 7' \
-    -H 'Mex-OSName: x86' -H 'Mex-OSVersion: 6.1'
+    -H 'Mex-OSName: x86' -H 'Mex-OSVersion: 6.1')
+
+  output_console "Response" "$response"
 }
 
 create_token
