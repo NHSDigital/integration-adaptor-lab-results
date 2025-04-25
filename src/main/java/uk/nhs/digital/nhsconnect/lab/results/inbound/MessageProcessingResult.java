a7ebf80b40db64d5ddc7072921eacc26ab3ef478
MartinWheelerMT <martin.wheeler@madetech.com>
2025-04-25 11:43:31 +0100

    NIAD-3321: Bump Java Version to 21
    * Suppress spotbugs warning for Bundle which is not accessed concurrently

--- src/main/java/uk/nhs/digital/nhsconnect/lab/results/inbound/MessageProcessingResult.java
+++ src/main/java/uk/nhs/digital/nhsconnect/lab/results/inbound/MessageProcessingResult.java
@@ -1,5 +1,6 @@
 package uk.nhs.digital.nhsconnect.lab.results.inbound;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.hl7.fhir.dstu3.model.Bundle;
@@ -10,13 +11,15 @@ import uk.nhs.digital.nhsconnect.lab.results.model.edifact.Message;
 public abstract class MessageProcessingResult {
     private final Message message;
 
+    // Suppressing this warning as false positive for Bundle which is not accessed concurrently.
+    @SuppressFBWarnings("EI_EXPOSE_REP2")
     @Getter
     public static final class Success extends MessageProcessingResult {
         private final Bundle bundle;
 
         public Success(Message message, Bundle bundle) {
             super(message);
-            this.bundle = bundle;
+            this.bundle = bundle.copy();
         }
     }
 
