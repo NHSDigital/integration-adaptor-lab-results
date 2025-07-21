051e27ed153f36bb93b41f6509f1c032021c3637
Alex-Nita <127960028+Alex-Nita@users.noreply.github.com>
2025-07-21 15:28:58 +0100

    Create download_message.py

---
+++ release/tests/download_message.py
@@ -0,0 +1,44 @@
+from proton import Message, Timeout
+from proton.utils import BlockingConnection
+from proton.reactor import DurableSubscription
+
+
+class BlockingQueueAdaptor(object):
+    """
+    Allows blocking reads from an AMQP message queue
+    """
+
+    def __init__(self):
+        self.username = 'admin'
+        self.password = 'admin'
+        self.queue_url = queue_url
+        self.queue_name = queue_name
+        print(self.get_next_message_on_queue())
+
+    def get_next_message_on_queue(self) -> Message:
+        """
+        Gets the next message from the queue
+        :return: Message read from queue
+        """
+        connection = BlockingConnection(self.queue_url, user=self.username, password=self.password)
+        receiver = connection.create_receiver(self.queue_name, options=DurableSubscription())
+        message = receiver.receive(timeout=30)
+        receiver.accept()
+        connection.close()
+
+        return message
+
+    def drain(self):
+        """
+        Drain the queue to prevent test failures caused by previous failing tests not ack'ing all of their messages
+        """
+        connection = BlockingConnection(self.queue_url, user=self.username, password=self.password)
+        receiver = connection.create_receiver(self.queue_name, options=DurableSubscription())
+        try:
+            while True:
+                receiver.receive(timeout=1)
+                receiver.accept()
+        except Timeout:
+            pass
+        finally:
+            connection.close()
