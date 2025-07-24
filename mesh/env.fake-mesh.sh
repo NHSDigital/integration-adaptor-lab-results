c721a9c6f6b3a1d4cdd12a0850b27d6941b22b69
Yasmin Morshed <77233981+morsh3d@users.noreply.github.com>
2025-07-24 07:51:48 +0100

    NIAD-3310: Update documentation and fix errors (#206)
    * Replace hscic in URLs with nhsengland as hscic no longer works
    
    * Apply minor formatting changes
    
    * Add fixme tags to broken links
    
    * Merge in Martin's changes to fix table borders and remove whitespace links
    
    * Update Robo 3T to Studio 3T in instructions as Robo 3T has been acquired
    
    * Standardise GitHub capitalisation in README
    
    * Fix broken anchor link to Operating.md
    
    * Bump Java version to 21 in VNP README
    
    * Fix small spelling issue
    
    * Update link/location to the MESH API documentation. Fix small typo in env.example.sh file.
    
    * Add mesh-sandbox to README as fake-mesh is no longer maintained
    
    * Add warning about CURL_FLAGS -k if left active
    
    * Update link to deployment repo as previous link was archived in Aug 2024 and is no longer maintained
    
    * Update AMQP specification link to the OASIS source to resolve connection not private browser warnings
    
    * Fix 404 link using correct file name in send_message.sh script. Fix small typos.
    
    * Improve documentation for sending messages to lab_results_ack_mailbox.
    
    * Update CURL_FLAGS warning when using -k
    
    * Improve documentation for sending messages to lab_results_ack_mailbox.
    
    * Add missing line to make script work
    
    * Fix broken links by adding pathology_3
    
    * Update the mesh script to source the env variables
    
    * Fix issue in mesh script to allow sending of multiple messages
    
    * Pushed fix for download in the python connectivity test script.
    
    * Remove .DS_Store files
    
    * Remove mention of top-level folder in readme instructions in release/tests folder
    
    ---------
    
    Co-authored-by: Chiara <chiara.mapelli@madetech.com>
    Co-authored-by: Alex-Nita <127960028+Alex-Nita@users.noreply.github.com>

--- mesh/env.fake-mesh.sh
+++ mesh/env.fake-mesh.sh
@@ -16,11 +16,13 @@ export HOST='localhost:8829'
 # path to the file containing the fake-mesh private key.
 export MESH_ENDPOINT_PRIVATE_KEY="./fakemesh.ca.key.pem"
 
-# path the file containg the fake-mesh endpoint certificate.
+# path the file containing the fake-mesh endpoint certificate.
 export MESH_ENDPOINT_CERT="./fakemesh.ca.cert.pem"
 
 # (optional) provide different flags / options for the curl command
 #export CURL_FLAGS="-s -i -k -o /dev/null"
+# WARNING: 'unset CURL_FLAGS' or close terminal immediately after use to avoid
+# unintended security risks (due to '-k') in subsequent 'curl' commands.
 
 # WorkflowID used to send MESH messages.
 # Can be either PATH_MEDRPT_V3, PATH_MEDRPT_V3_ACK, SCRN_BCS_MEDRPT_V4 or SCRN_BCS_MEDRPT_V4_ACK.
